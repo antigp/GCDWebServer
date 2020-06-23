49632ccf420126d56344dde841dfe4fa04f6b729
Eugene Antropov <antigp@gmail.com>
2020-06-23 22:31:25 +0300

    Make library dynamic

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "GCDWebServer",
     products: [
-        .library(name: "GCDWebServer", targets: ["GCDWebServer"]),
+        .library(name: "GCDWebServer", type: .dynamic, targets: ["GCDWebServer"]),
     ],
     targets: [
         .target(
